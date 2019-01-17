615795ae30ac7479d1b144ca2f20d2e47ee45441
zulune <mars.xaker@gmail.com>
2019-01-17 18:02:22 +0200

    Check user method scaleton

--- authenticate.py
+++ authenticate.py
@@ -8,6 +8,13 @@ bot = telebot.TeleBot(token)
 
 class Auth:
 
+    def check_user(message):
+        url = 'http://localhost:8000/api/v2/user/'
+        r = requests.get(url)
+        if (message.id in r.text['client']['telegram']) {
+            print('Success')
+        }
+
     def create_user(message):
         url = 'http://localhost:8000/api/v2/user'
         req = requests.get(url)
